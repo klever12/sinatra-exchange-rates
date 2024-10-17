ec3abdd5a4c08bba2fc2fe94f6a3a5443cc96ae4
Klever Gopar <20468297+klever12@users.noreply.github.com>
2024-10-17 07:32:52 +0000

    had to fork project again because tests were not working in previous fork

--- app.rb
+++ app.rb
@@ -1,9 +1,51 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "dotenv/load"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+
+  @raw_response = HTTP.get(api_url)
+
+  @raw_string = @raw_response.to_s
+
+  @parsed_data = JSON.parse(@raw_string)
+
+  @currencies = @parsed_data.fetch("currencies")
+
+  erb(:homepage)
+end
+
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+
+  @raw_response = HTTP.get(api_url)
+
+  @raw_string = @raw_response.to_s
+
+  @parsed_data = JSON.parse(@raw_string)
+
+  @currencies = @parsed_data.fetch("currencies")
+
+  erb(:first_currency)
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+
+  @raw_response = HTTP.get(api_url)
+
+  @raw_string = @raw_response.to_s
+
+  @parsed_data = JSON.parse(@raw_string)
+
+  @amount = @parsed_data.fetch("result")
+
+  erb(:second_currency)
 end
